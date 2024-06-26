fa596c22752f4566ae4e34df09eea2c2f4fb04ac
Ethan Wang <ethan_wang2@my.vcccd.edu>
2024-06-26 14:25:35 -0700

    First

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ def main():
     number1 = random.randint(0, 100)
     number2 = random.randint(0, 100)
     number3 = random.randint(0, 100)
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
 
+    min_value = 0
+    
+    if number1 < number2 and number1 < number3:
+        min_value = number1
+    elif number2 < number1 and number2 < number3:
+        min_value = number2
+    else:
+        min_value = number3    
+
+    print(f'{min_value}')
     ########################################
     # Do not delete the return statement
     ########################################
